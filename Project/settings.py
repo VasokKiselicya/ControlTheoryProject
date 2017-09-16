ce20e14df11f8c6981a3d74e6a65e6bd0f423d8b
VasokKiselicya <v.kiselicya021036@gmail.com>
2017-09-16 15:00:04 +0300

    Heroku Fix!

--- Project/settings.py
+++ Project/settings.py
@@ -165,9 +165,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # if DEBUG:
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'staticfiles'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'staticfiles'),
+# )
 # else:
 
 
