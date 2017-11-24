715cd6e565a9752481ca3d13f7bc992771164e84
VasokKiselicya <v.kiselicya021036@gmail.com>
2017-11-24 14:20:50 +0200

    Add Href To Menu On Landing!

--- core/service/views.py
+++ core/service/views.py
@@ -2,6 +2,7 @@ from django.views import View
 from django.shortcuts import render
 from django.utils.translation import get_language
 from db.models import Category, Product, Unit, Article
+from core.templatetags.app_tags import slugify
 
 
 class MenuView(View):
@@ -10,12 +11,17 @@ class MenuView(View):
     def get(self, request):
         unit_names = dict(Unit.objects.all().values_list('id', 'short_name'))
         categories = list(Category.objects.all().values("id", "name", "description", "photo").order_by("id"))
+        active_category = request.GET.get("category", "")
         for cat in categories:
+            cat['slug'] = slugify(cat['name'])
+            cat['active'] = cat['slug'] == active_category
             cat["products"] = list(Product.objects.filter(category_id=cat["id"]).order_by("name").values())
             for product in cat["products"]:
                 product['unit_name'] = unit_names.get(product['unit_id'], '')
                 product["ingredients"] = ', '.join(Product.objects.get(id=product["id"]).product_ingredients.all()
                                                    .values_list('ingredient__name', flat=True))
+        if not active_category:
+            categories[0].update({'active': True})
         return render(request, self.template_name, {"section": "menu", "categories": categories})
 
 
