e175123157e005dbef8d9980780cf3a376fb66aa
VasokKiselicya <v.kiselicya021036@gmail.com>
2017-12-02 20:43:54 +0200

    Add Language Middleware For Fix Translation!

--- Project/settings.py
+++ Project/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "Project.middleware.LocaleMiddleware"
 ]
 
 ROOT_URLCONF = 'Project.urls'
